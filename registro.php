69d0014c55e4364ed1eb4a2870ee3df3503d34ab
SamuelArrataPluas <srarrata@est.istg.edu.ec>
2025-01-30 12:11:42 +0100

    actualizacionregistroyestilo30012025_12:11pm
    aaaaaaaaaaa

--- registro.php
+++ registro.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
-    <link rel="stylesheet" href="./css/Style.css?v=1.7">
+    <link rel="stylesheet" href="./css/Style.css?v=1.8">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/4587fd54e5.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <br><br><br><br>
@@ -27,50 +28,41 @@
                             }
                         ?>
                         <!-- grupo nombre completo -->
-                        <div class="input-group mb-3 input-group-sm" id="nombres">
+                        <div class="formulario__grupo input-group mb-3 input-group-sm" id="nombre_apellido">
                             <i class="input-group-text bi bi-person-plus-fill"></i>
                             <div class="formulario__grupo-input">
-                                <input type="text" class="form-control" placeholder="Nombre completo" name="nombres">
+                                <input type="text" class="formulario__input" placeholder="Nombre y apellido" name="nombre_apellido">
                                 <i class="formulario__validacion-estado fa-solid fa-circle-xmark"></i>
                             </div>
-                            <p class="formulario__input-error">Escriba su nombre completo porfavor</p>
+                            <p class="formulario__input-error">Escriba su nombre y apellido completo porfavor</p>
                         </div>
 
-                        <!-- grupo apellidos -->
-                        <div class="input-group mb-3 input-group-sm" id="apellidos">
-                            <i class="input-group-text bi bi-person-plus-fill"></i>
-                            <div class="formulario__grupo-input">
-                                <input type="text" class="form-control" placeholder="Apellido completo" name="apellidos">
-                                <i class="formulario__validacion-estado fa-solid fa-circle-xmark"></i>
-                            </div>
-                            <p class="formulario__input-error">Escriba su apellido completo completo porfavor</p>
-                        </div>
                         <!-- grupo correo electronico -->
-                        <div class="input-group mb-3 input-group-sm" id="gmail">
+                        <div class="formulario__grupo input-group mb-3 input-group-sm" id="correo">
                             <i class="input-group-text bi bi-envelope-fill"></i>
                             <div class="formulario__grupo-input">
-                                <input type="email" class="form-control" placeholder="Correo electrónico" name="gmail">
+                                <input type="email" class="formulario__input" placeholder="Correo electrónico" name="correo">
                                 <i class="formulario__validacion-estado fa-solid fa-circle-xmark"></i>
                             </div>
                             <p class="formulario__input-error">Correo solo puede contener letras, numeros, puntos, guiones y guion bajo.</p>
                         </div>
-                        <!-- grupo usuario -->
-                        <div class="input-group mb-3 input-group-sm" id="usuario">
-                            <i class="input-group-text bi bi-person-circle"></i>
-                            <div class="formulario__grupo-input">
-                                <input type="text" class="form-control" placeholder="Usuario" name="usuario">
-                                <i class="formulario__validacion-estado fa-solid fa-circle-xmark"></i>
-                            </div>
-                            <p class="formulario__input-error">El usuario puede tener entre 4 a 16 digitos, puede contener numeros, letras y guion bajo.</p>
-                        </div>
                         <!-- grupo contraseña -->
-                        <div class="input-group mb-3 input-group-sm" id="password">
+                        <div class="formulario__grupo input-group mb-3 input-group-sm" id="password">
                             <i class="input-group-text bi bi-person-lock"></i>
                             <div class="formulario__grupo-input">
-                                <input type="password" class="form-control" placeholder="Contraseña" name="password">
+                                <input type="password" class="formulario__input" placeholder="Contraseña" name="password">
                                 <i class="formulario__validacion-estado fa-solid fa-circle-xmark"></i>
                             </div>
                             <p class="formulario__input-error">La contraseña tiene que tener entre 4 y 12 caracteres</p>
+                        </div>
+                        <!-- grupo confirmar contraseña -->
+                        <div class="formulario__grupo input-group mb-3 input-group-sm" id="password2">
+                            <i class="input-group-text bi bi-person-lock"></i>
+                            <div class="formulario__grupo-input">
+                                <input type="password" class="formulario__input" placeholder="Confirmar contraseña" name="password2">
+                                <i class="formulario__validacion-estado fa-solid fa-circle-xmark"></i>
+                            </div>
+                            <p class="formulario__input-error">Ambas contraseñas deben ser iguales</p>
                         </div>
                         <button type="submit" class="btn btn-success btn-sm" name="registrar">Crear cuenta</button>
                         <br>
@@ -90,6 +82,5 @@
     </div>
     
     <script src="./js/validar.js"></script>
-    <script src="https://kit.fontawesome.com/2c36e9b7b1.js" crossorigin="anonymous"></script>
 </body>
 </html>
